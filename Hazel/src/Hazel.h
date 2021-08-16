1675d07598bbbae1406eef4290ded77c5fc33ec1
WhoLeb <gleb_lyah@mail.ru>
2021-08-16 18:31:19 +0300

    Added logging system

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -2,7 +2,7 @@
 
 #include <stdio.h>
 #include "Hazel/Application.h"
-
+#include "Hazel/Log.h"
 
 //entry point
 #include "Hazel/EntryPoint.h"
--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#pragma once
-
 #ifdef HZ_PLATFORM_WINDOWS
 #ifdef HZ_BUILD_DLL
 #define HAZEL_API __declspec(dllexport)
